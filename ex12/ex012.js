1aa2ebf08920843204bbd4f5d7bbb2cf00c3eab1
Eduastras <eduastras@outlook.com>
2025-12-26 02:34:27 -0300

    ex012 att

---
+++ ex12/ex012.js
@@ -0,0 +1,15 @@
+let agora = new Date()
+let hora = agora.getHours()
+
+console.log(`Agora são exatamente ${hora} horas.`)
+
+if (hora < 12 && hora >= 0o5) {
+    console.log("Bom Dia!")
+} else if (hora < 18 && hora > 12) {
+    console.log("Boa Tarde!")
+} else if (hora > 18 && hora < 23) {
+    console.log("Boa Noite!")
+} else {
+    console.log("Boa Madrugada!")
+}
+ 
